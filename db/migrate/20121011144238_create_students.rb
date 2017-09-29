638bd47a7c6877fbc926b41b15f5b3d772668b5c
KevinSia <kevinsiakeemun@gmail.com>
2017-09-29 10:46:56 +0800

    Update 20121011144238_create_students.rb

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -2,7 +2,7 @@ require_relative '../config'
 
 # this is where you should use an ActiveRecord migration to
 
-class CreateStudents < ActiveRecord::Migration
+class CreateStudents < ActiveRecord::Migration[5.0]
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
     create_table :students do |t|
